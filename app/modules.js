24acff24335603a235d5b52997a4a4b32d3d703b
thinktt <github@thinktt.e4ward.com>
2014-10-16 14:22:59 -0500

    working on objects

--- app/modules.js
+++ app/modules.js
@@ -1,10 +1,35 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
-define(function() {
-  return {
-    createModule : function(str1, str2) {
 
-    }
-  };
+var  createModule = function(str1, str2) {
+	var sayIt,
+			mod = {};
+
+	mod.greeting = str1;
+	mod.name = str2; 
+
+	mod.sayIt = function () {
+		return mod.greeting + ', ' + mod.name;
+	};
+
+	return mod;
+};
+
+define(function() {
+  return {createModule: createModule};
 });
 
+
+var testMod = createModule('Howdy', 'Bob');
+
+console.log(testMod.sayIt());
+console.log(testMod.greeting);
+console.log(testMod.name);
+
+
+testMod.greeting = 'Yo';
+testMod.name = 'Adrian';
+
+console.log(testMod.sayIt());
+console.log(testMod.greeting);
+console.log(testMod.name);
--- app/objects.js
+++ app/objects.js
@@ -3,8 +3,7 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-  		console.log(fn);
-  		console.log(obj);
+
 
     },
 
