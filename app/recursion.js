d97279403a692d96cb080fbde6fc792e2b0fb19a
thinktt <github@thinktt.e4ward.com>
2014-10-17 18:51:05 -0500

    finished with regexes

--- app/recursion.js
+++ app/recursion.js
@@ -3,6 +3,8 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     listFiles: function(data, dirName) {
+    	console.log(data.files);
+    	console.log(dirName);
 
     },
 
--- app/regex.js
+++ app/regex.js
@@ -1,5 +1,5 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
-//use regex pal as always
+//used regex pal to help find most of these
 define(function() {
   return {
     containsNumber : function(str) {
@@ -18,16 +18,18 @@ define(function() {
     },
 
     captureThreeNumbers : function(str) {
-        //MDN spec
+        //MDN spec to find exec()
         var first3Nums =  /\d{3}/.exec(str) || false;
         if(first3Nums) first3Nums = first3Nums[0];
         return first3Nums;
     },
 
     matchesPattern : function(str) {
-
+        var isMatch = /^[\d]{3}-[\d]{3}-[\d]{4}$/.test(str);
+        return isMatch;
     },
     isUSD : function(str) {
+        return /^\$(?:[\d]{1,3})(,[\d]{3})*(\.[\d]{2})?$/.test(str);
 
     }
   };
