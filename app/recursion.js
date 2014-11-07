bb6bba16b41e894f668fe5a3920880ac2b43fecf
thinktt <github@thinktt.e4ward.com>
2014-11-07 16:54:42 -0600

    change tabs to spaces

--- app/recursion.js
+++ app/recursion.js
@@ -5,61 +5,61 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     listFiles: function(data, dirName) {
-    	var i, files = [];
+      var i, files = [];
 
-    	if(dirName) {
-    		return this.getDirFiles(data, dirName);
-    	}
+      if(dirName) {
+        return this.getDirFiles(data, dirName);
+      }
 
-    	for(i=0; i < data.files.length; i++) {
-    		if(typeof data.files[i] === 'string') {
-    			files.push(data.files[i]);
-    		} else {
-    			files = files.concat(this.listFiles(data.files[i]));
-    		}
-    	}
-    	return files;
+      for(i=0; i < data.files.length; i++) {
+        if(typeof data.files[i] === 'string') {
+          files.push(data.files[i]);
+        } else {
+          files = files.concat(this.listFiles(data.files[i]));
+        }
+      }
+      return files;
     },
 
     getDirFiles: function(data, dirName) {
-    	var i, files;
-    	if(data.dir === dirName) {
-    		return this.listFiles(data);
-    	}
+      var i, files;
+      if(data.dir === dirName) {
+        return this.listFiles(data);
+      }
 
-    	for(i=0; i < data.files.length; i++) {
-    		if(typeof data.files[i] === 'object') {
-    			return this.getDirFiles(data.files[i], dirName);
-    		}
-    	}
+      for(i=0; i < data.files.length; i++) {
+        if(typeof data.files[i] === 'object') {
+          return this.getDirFiles(data.files[i], dirName);
+        }
+      }
     },
 
     permute: function(arr) {
-			
-			function getPermutation(list) {
-				var arr = list.slice();
-				var permutations;
-				var permsList = [];
-				var current; 
-				var i, j;
+      
+      function getPermutation(list) {
+        var arr = list.slice();
+        var permutations;
+        var permsList = [];
+        var current; 
+        var i, j;
 
-				if(list.length === 1) return [arr];
+        if(list.length === 1) return [arr];
 
-				for(i=0; i<list.length; i++) { 
-					current = list[i];
-					arr = list.slice();
-					arr.splice(i,1);
+        for(i=0; i<list.length; i++) { 
+          current = list[i];
+          arr = list.slice();
+          arr.splice(i,1);
 
-					permutations = getPermutation(arr); 
-					for(j=0; j<permutations.length; j++) {
-						permutations[j].unshift(current);
-						permsList.push(permutations[j]);
-					}
-				}
-				return permsList;
-			}
-    	
-    	return getPermutation(arr);
+          permutations = getPermutation(arr); 
+          for(j=0; j<permutations.length; j++) {
+            permutations[j].unshift(current);
+            permsList.push(permutations[j]);
+          }
+        }
+        return permsList;
+      }
+      
+      return getPermutation(arr);
     }
   };
 });
