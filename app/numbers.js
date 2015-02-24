0102424b4c8d8a43b63e3e58c19588d3084fca07
thinktt <github@thinktt.e4ward.com>
2015-02-23 23:55:37 -0600

    work on binary number problems

--- app/numbers.js
+++ app/numbers.js
@@ -2,15 +2,22 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define(function() {
   return {
+
+    //SO and MDN help
     valueAtBit: function(num, bit) {
-
+        --bit; 
+        return ( ((1 << bit) & num)  >> bit);
     },
 
+    //MDN reference
     base10: function(str) {
+        return parseInt(str, 2);
 
     },
 
+    //SO help
     convertToBinary: function(num) {
+        return ('000000000' + num.toString(2)).substr(-8);
 
     },
 
