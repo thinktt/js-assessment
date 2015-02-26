197f8c350a86e9b2dd17f8a3421435ce0535d252
thinktt <github@thinktt.e4ward.com>
2015-02-25 19:31:05 -0600

    finished number section

--- app/count.js
+++ app/count.js
@@ -1,8 +1,23 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
+var nums = [1,2,3,4]; 
+
 define(function () {
   return {
     count : function (start, end) {
+    	var i, count, interval;  
+
+			i=start;
+			//nums = [start];
+
+			count = function(){
+				i++;
+				//nums.push(i);
+				console.log(nums);
+				if(i>end) clearInterval(interval);
+			};
+
+			interval = setInterval(count, 100);
     }
   };
 });
--- app/numbers.js
+++ app/numbers.js
@@ -21,8 +21,31 @@ define(function() {
 
     },
 
+    //variation on SO finds and MDN ref
     multiply: function(a, b) {
+        var countDecimals, aDecLength, bDecLength, exp, powOfTen, product;
 
+        countDecimals = function(num) { 
+            var decPart, decLength; 
+            //converts number into a string then caoture the decimal part
+            decPart = num.toString(10).match(/\.(.*)/);
+            //if something was captured  get it's lenght otherwise length is 0
+            if(decPart) decLength = decPart[1].length;
+                else decLength = 0; 
+
+            return decLength; 
+        };
+
+        aDecLength = countDecimals(a);
+        bDecLength = countDecimals(b);
+        //find the longest decimal lengh to use as power of ten exponent
+        exp = aDecLength > bDecLength ? aDecLength : bDecLength;
+        powOfTen = Math.pow(10, exp);
+        a = powOfTen * a;
+        b = powOfTen * b;
+        product = (a*b)/(powOfTen * powOfTen);
+
+        return product; 
     }
   };
 });
