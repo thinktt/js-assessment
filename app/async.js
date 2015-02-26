9988dd3b8c77cdc3288acd6d484941b44170979b
thinktt <github@thinktt.e4ward.com>
2015-02-25 23:26:46 -0600

    all assesments completed with passing test
    still need to go back and look at the functions and do them correctly

--- app/async.js
+++ app/async.js
@@ -1,18 +1,31 @@
+/*globals Promise:true*/
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
-var globey;
 
 define([ 'jquery' ], function($) {
   return {
     async : function(value) {
+    	var promise; 
+    	promise = new Promise(function(resolve, reject){
+    		resolve(value);
+    	}); 
+    	return promise; 
 
     },
 
     manipulateRemoteData : function(url) {
+    	var names = [], promise;
+    	promise = new Promise(function(resolve, reject) {
+	    	$.getJSON(url, function(data){
+	    		$.each(data.people, function (key, value) {
+	    			names.push(value.name);
+	    		});
+	    		resolve(names.sort());
 
-    	$.getJSON(url, function(data){
-    		globey = data.people; 
+	    	});
+
     	});
 
+    	return promise; 
     }
   };
 });
